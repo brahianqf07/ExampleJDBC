45ebac46ef951d8a392a7ea77abb16a7ea8eda76
Brahian Quintero <brahianquintero07@gmail.com>
2025-03-20 07:32:42 -0500

    Agregar pruebas para (insertar, actualizar, eliminar, leer) de la tabla key
    Implementa la clase TestKey para validar las operaciones:
    Insertar: Agrega una nueva llave.
    Actualizar: Modifica una llave existente.
    Eliminar: Borra una llave por id.
    Consultar todos: Lista todas las llaves.
    Consultar por id: Busca una llave especifica.
    Imprime los resultados en consola para verificar el funcionamiento.

--- ExampleJDBC/src/main/java/co/edu/sena/examplejdbc/test/TestKey.java
+++ ExampleJDBC/src/main/java/co/edu/sena/examplejdbc/test/TestKey.java
@@ -6,6 +6,7 @@ package co.edu.sena.examplejdbc.test;
 
 import co.edu.sena.examplejdbc.bd.DBKey;
 import co.edu.sena.examplejdbc.model.Key;
+import java.util.List;
 
 /**
  *
@@ -15,7 +16,32 @@ public class TestKey {
     public static void main(String[] args) {
         DBKey dBKey = new DBKey();
         //Insertar:
-        Key key = new Key(5,"mi casa","Sala2", 2, "Llavero");
+        Key key = new Key(5,"BICENTENARIO","Sala 2", 2, "Llavero");
         dBKey.insert(key);
+        //Actualizar
+        key.setId(5);
+        key.setName("INFORMATICA 3");
+        key.setRoom("salón 202");
+        key.setCount(1);
+        key.setObservation("Llave sola");
+        dBKey.update(key);
+        //Eliminar
+        dBKey.delete(5);
+        //consultar
+        List<Key> listKey = dBKey.findALL();
+        for (Key e : listKey) {
+            System.out.println("id: " + e.getId() + "name: " + e.getName() + "room: " + e.getRoom()
+            + "count: " + e.getCount() + "observation: " + e.getObservation());
+        }
+        //Comsultar por id
+        System.out.println("**********************************");
+        Key key2 = dBKey.findById(1);
+        if (key2 != null) 
+        {
+            System.out.println("id: " + key2.getId() + "name: " + key2.getName() + "room: " + key2.getRoom()
+            + "count: " + key2.getCount() + "observation: " + key2.getObservation());  
+        }
+          
     }
 }
+
